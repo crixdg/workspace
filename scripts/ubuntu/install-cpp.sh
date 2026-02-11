20b959b91ed0618cad52d6b6973e4192d91f70f9
Crix Dang <crix.dang@gmail.com>
2026-02-11 21:43:57 +0700

    up Wed Feb 11 02:43:57 PM UTC 2026

--- scripts/ubuntu/install-cpp.sh
+++ scripts/ubuntu/install-cpp.sh
@@ -4,9 +4,6 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/check-os.sh"
 
-sudo apt update
-sudo apt install build-essential -y
-
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt update
