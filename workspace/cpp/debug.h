9936b4ee45790601678ebe59e3f052c8cf0f00e5
Crix Dang <crix.dang@gmail.com>
2026-03-23 03:03:22 +0700

    up Sun Mar 22 08:03:22 PM UTC 2026

--- workspace/cpp/debug.h
+++ workspace/cpp/debug.h
@@ -5,208 +5,166 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T> void __info(const T &t);
+template <typename T> void __dbg(const T &t);
 
-template <typename T, typename V> void __info(const pair<T, V> &t);
-template <typename T, typename V, typename K> void __info(const tuple<T, V, K> &t);
+template <typename T, typename V> void __dbg(const pair<T, V> &t);
+template <typename... Ts> void __dbg(const tuple<Ts...> &t);
 
-template <typename T> void __info(const stack<T> &t);
-template <typename T> void __info(const queue<T> &t);
+template <typename K, typename V> void __dbg(const map<K, V> &t);
+template <typename K, typename V> void __dbg(const unordered_map<K, V> &t);
+template <typename K, typename V> void __dbg(const multimap<K, V> &t);
+template <typename K, typename V> void __dbg(const unordered_multimap<K, V> &t);
 
-template <typename T> void __info(const priority_queue<T> &t);
-template <typename T> void __info(const priority_queue<T, vector<T>, greater<T>> &t);
+template <typename T> void __dbg(const stack<T> &t);
+template <typename T> void __dbg(const queue<T> &t);
 
-inline void __info(short t) { cerr << t; }
-inline void __info(unsigned short t) { cerr << t; }
-inline void __info(int t) { cerr << t; }
-inline void __info(long t) { cerr << t; }
-inline void __info(long long t) { cerr << t; }
-inline void __info(unsigned int t) { cerr << t; }
-inline void __info(unsigned long t) { cerr << t; }
-inline void __info(unsigned long long t) { cerr << t; }
-inline void __info(float t) { cerr << t; }
-inline void __info(double t) { cerr << t; }
-inline void __info(long double t) { cerr << t; }
-inline void __info(char t) { cerr << '\'' << t << '\''; }
-inline void __info(const char *t) { cerr << '\"' << t << '\"'; }
-inline void __info(const string &t) { cerr << '\"' << t << '\"'; }
-inline void __info(bool t) { cerr << (t ? "true" : "false"); }
+template <typename T> void __dbg(const priority_queue<T> &t);
+template <typename T> void __dbg(const priority_queue<T, vector<T>, greater<T>> &t);
 
-inline string to_string(__int128_t t);
-inline void __info(__int128_t t) { cerr << to_string(t); }
+inline void __dbg(short t) { cerr << t; }
+inline void __dbg(unsigned short t) { cerr << t; }
+inline void __dbg(int t) { cerr << t; }
+inline void __dbg(long t) { cerr << t; }
+inline void __dbg(long long t) { cerr << t; }
+inline void __dbg(unsigned int t) { cerr << t; }
+inline void __dbg(unsigned long t) { cerr << t; }
+inline void __dbg(unsigned long long t) { cerr << t; }
+inline void __dbg(float t) { cerr << t; }
+inline void __dbg(double t) { cerr << t; }
+inline void __dbg(long double t) { cerr << t; }
+inline void __dbg(char t) { cerr << '\'' << t << '\''; }
+inline void __dbg(const char *t) { cerr << '\"' << t << '\"'; }
+inline void __dbg(const string &t) { cerr << '\"' << t << '\"'; }
+inline void __dbg(bool t) { cerr << (t ? "true" : "false"); }
 
-inline void __print() { cerr << '\n'; }
+inline string __i128_to_string(__int128_t t);
+inline void __dbg(__int128_t t) { cerr << __i128_to_string(t); }
 
-template <typename T, typename... V> inline void __print(T t, V... v) {
-  __info(t);
+inline void __dbg_print() { cerr << '\n'; }
+
+template <typename T, typename... V> inline void __dbg_print(T t, V... v) {
+  __dbg(t);
   if (sizeof...(v)) {
     cerr << ", ";
   }
-  __print(v...);
+  __dbg_print(v...);
 }
 
 #define debug(t...)                                                                                                                                  \
   cerr << "\e[90m" << __func__ << "::" << __LINE__ << ": ";                                                                                          \
-  __print(t);                                                                                                                                        \
+  __dbg_print(t);                                                                                                                                    \
   cerr << "\e[39m";
 
-inline void __test__() {
-  short sh = SHRT_MAX;
-  debug(sh);
-  unsigned short ush = USHRT_MAX;
-  debug(ush);
-
-  int i = INT_MAX;
-  debug(i);
-  long l = LONG_MAX;
-  debug(l);
-  long long ll = LLONG_MAX;
-  debug(ll);
-  __int128_t i128 = LLONG_MAX;
-  debug(i128);
-
-  unsigned int ui = UINT_MAX;
-  debug(ui);
-  unsigned long ul = ULONG_MAX;
-  debug(ul);
-  unsigned long long ull = ULLONG_MAX;
-  debug(ull);
-
-  float f = FLT_MAX;
-  debug(f);
-  double d = DBL_MAX;
-  debug(d);
-  long double ld = LDBL_MAX;
-  debug(ld);
-
-  char c = 'a';
-  debug(c);
-  const char *cs = "hello";
-  debug(cs);
-  string s = "hello world";
-  debug(s);
-
-  bool b = true;
-  debug(b);
-
-  pair<int, int> p = {1, 2};
-  debug(p);
-  tuple<int, int, int> tpl = {1, 2, 3};
-  debug(tpl);
-
-  vector<int> v = {1, 2, 3};
-  debug(v);
-  array<int, 3> a = {1, 2, 3};
-  debug(a);
-
-  deque<int> dq = {1, 2, 3};
-  debug(dq);
-  list<int> lt = {1, 2, 3};
-  debug(lt);
-
-  stack<int> stk;
-  stk.push(1);
-  stk.push(2);
-  stk.push(3);
-  debug(stk);
-
-  queue<int> qu;
-  qu.push(1);
-  qu.push(2);
-  qu.push(3);
-  debug(qu);
-
-  unordered_set<int> us = {1, 2, 3};
-  set<int> st = {1, 2, 3};
-  multiset<int> ms = {1, 2, 3};
-  debug(us, st, ms);
-
-  map<int, int> m = {{1, 2}, {3, 4}};
-  unordered_map<int, int> um = {{1, 2}, {3, 4}};
-  multimap<int, int> mm = {{1, 2}, {3, 4}};
-  debug(m, um, mm);
-
-  priority_queue<int> max_heap;
-  max_heap.push(1);
-  max_heap.push(2);
-  max_heap.push(3);
-  debug(max_heap);
-
-  priority_queue<int, vector<int>, greater<int>> min_heap;
-  min_heap.push(1);
-  min_heap.push(2);
-  min_heap.push(3);
-  debug(min_heap);
-}
-
-template <typename T> void __info(const T &t) {
+template <typename T> void __dbg(const T &t) {
   int f = 0;
   cerr << '[';
   for (auto &i : t) {
-    cerr << (f++ ? ", " : ""), __info(i);
+    cerr << (f++ ? ", " : ""), __dbg(i);
   }
   cerr << "]";
 }
 
-template <typename T> void __info(const queue<T> &t) {
+template <typename T, typename V> void __dbg(const pair<T, V> &t) {
+  cerr << '(';
+  __dbg(t.first);
+  cerr << ", ";
+  __dbg(t.second);
+  cerr << ')';
+}
+
+template <size_t I = 0, typename... Ts> void _dbg_tuple(const tuple<Ts...> &t) {
+  if constexpr (I < sizeof...(Ts)) {
+    if constexpr (I > 0) {
+      cerr << ", ";
+    }
+    __dbg(get<I>(t));
+    _dbg_tuple<I + 1>(t);
+  }
+}
+
+template <typename... Ts> void __dbg(const tuple<Ts...> &t) {
+  cerr << '(';
+  _dbg_tuple(t);
+  cerr << ')';
+}
+
+template <typename K, typename V> void __dbg(const map<K, V> &t) {
+  int f = 0;
+  cerr << '{';
+  for (auto &[k, v] : t) {
+    cerr << (f++ ? ", " : ""), __dbg(k), cerr << ": ", __dbg(v);
+  }
+  cerr << '}';
+}
+
+template <typename K, typename V> void __dbg(const unordered_map<K, V> &t) {
+  int f = 0;
+  cerr << '{';
+  for (auto &[k, v] : t) {
+    cerr << (f++ ? ", " : ""), __dbg(k), cerr << ": ", __dbg(v);
+  }
+  cerr << '}';
+}
+
+template <typename K, typename V> void __dbg(const multimap<K, V> &t) {
+  int f = 0;
+  cerr << '{';
+  for (auto &[k, v] : t) {
+    cerr << (f++ ? ", " : ""), __dbg(k), cerr << ": ", __dbg(v);
+  }
+  cerr << '}';
+}
+
+template <typename K, typename V> void __dbg(const unordered_multimap<K, V> &t) {
+  int f = 0;
+  cerr << '{';
+  for (auto &[k, v] : t) {
+    cerr << (f++ ? ", " : ""), __dbg(k), cerr << ": ", __dbg(v);
+  }
+  cerr << '}';
+}
+
+template <typename T> void __dbg(const queue<T> &t) {
   int f = 0;
   cerr << '[';
   queue<T> tmp = t;
   while (!tmp.empty()) {
-    cerr << (f++ ? ", " : ""), __info(tmp.front()), tmp.pop();
+    cerr << (f++ ? ", " : ""), __dbg(tmp.front()), tmp.pop();
   }
   cerr << "]";
 }
 
-template <typename T> void __info(const stack<T> &t) {
+template <typename T> void __dbg(const stack<T> &t) {
   int f = 0;
   cerr << '[';
   stack<T> tmp = t;
   while (!tmp.empty()) {
-    cerr << (f++ ? ", " : ""), __info(tmp.top()), tmp.pop();
+    cerr << (f++ ? ", " : ""), __dbg(tmp.top()), tmp.pop();
   }
   cerr << "]";
 }
 
-template <typename T> void __info(const priority_queue<T> &t) {
+template <typename T> void __dbg(const priority_queue<T> &t) {
   int f = 0;
   cerr << '[';
   priority_queue<T> tmp = t;
   while (!tmp.empty()) {
-    cerr << (f++ ? ", " : ""), __info(tmp.top()), tmp.pop();
+    cerr << (f++ ? ", " : ""), __dbg(tmp.top()), tmp.pop();
   }
   cerr << "]";
 }
 
-template <typename T> void __info(const priority_queue<T, vector<T>, greater<T>> &t) {
+template <typename T> void __dbg(const priority_queue<T, vector<T>, greater<T>> &t) {
   int f = 0;
   cerr << '[';
   priority_queue<T, vector<T>, greater<T>> tmp = t;
   while (!tmp.empty()) {
-    cerr << (f++ ? ", " : ""), __info(tmp.top()), tmp.pop();
+    cerr << (f++ ? ", " : ""), __dbg(tmp.top()), tmp.pop();
   }
   cerr << "]";
 }
 
-template <typename T, typename V> void __info(const pair<T, V> &t) {
-  cerr << '(';
-  __info(t.first);
-  cerr << ", ";
-  __info(t.second);
-  cerr << ')';
-}
-
-template <typename T, typename V, typename K> void __info(const tuple<T, V, K> &t) {
-  cerr << '(';
-  __info(get<0>(t));
-  cerr << ", ";
-  __info(get<1>(t));
-  cerr << ", ";
-  __info(get<2>(t));
-  cerr << ')';
-}
-
-inline string to_string(__int128_t t) {
+inline string __i128_to_string(__int128_t t) {
   if (t == 0) {
     return "0";
   }
